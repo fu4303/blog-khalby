d6275637381bab60070be3bf0f1705dd2ebbd4f0
Glitch (khaleelgibran-blog) <none>
2021-05-15 12:49:13 +0000

    (feat) use eleventy for a faster blog

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ## my blog :)
 
-after many versions of this blog, i've finally decided to settle with a vue + notion combo. i write blog posts in notion, and they appear here. yaay! if you want to know how i did that, check out [this article](https://benborgers.com/posts/notion-blog).
+after soooooo many iterations of the same blog, here's a (hopefully) final one with eleventy with a size less than 512kb.
+i write blog posts in notion, and they appear here. yaay! if you want to know how i did that, check out [this article](https://benborgers.com/posts/notion-blog).
 
 caught a mistake or have a question? open an issue/pull request or email me at hey@khaleelgibran.com
+
+_licensed under the MIT license_ 
+
+[![Remix on Glitch](https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg)](https://glitch.com/edit/#!/remix/khaleelgibran-blog)
---
+++ src/_data/posts.js
@@ -0,0 +1,57 @@
+const request = require("sync-request");
+const readingTime = require("reading-time");
+const slugify = require("slugify");
+const moment = require("moment");
+const md = require("markdown-it")({
+  html: true,
+  linkify: true,
+  typographer: true
+});
+
+require("dotenv").config();
+
+var res = request(
+  "GET",
+  `https://khaleelgibran-blog-notion.vercel.app/table?id=9676e5ba544740f58d0eb6404220f74c`,
+  {
+    headers: {
+      "user-agent": "aboutdavid.me/1.0"
+    }
+  }
+);
+
+res = JSON.parse(res.getBody("utf8"));
+res = res.filter(item => item.fields.Published == true);
+
+let posts = [];
+let i = 0;
+
+while (i < res.length) {
+  let post = res[i];
+  let html = request("GET", `https://khaleelgibran-blog-notion.vercel.app/html?id=${post.id}`, {
+    headers: {
+      "user-agent": "aboutdavid.me/1.0"
+    }
+  }).getBody("utf8");
+  let date = new Date(post.fields.Date.start_date);
+  let day = date.getDate();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let slug = post.fields.Slug;
+  
+  posts.push({
+    title: post.fields.Title,
+    date: moment(date.toISOString()).format("MMMM Do[,] YYYY"),
+    dateObject: date,
+    description: post.fields.Description || "",
+    readTime: readingTime(html).text,
+    slug: slug,
+    html: md.render(html),
+    url: `https://khaleelgibran-blog.glitch.me/post/${slug}`
+  });
+  i++;
+}
+
+module.exports = posts.sort(
+  (a, b) => new Date(b.dateObject) - new Date(a.dateObject)
+);
